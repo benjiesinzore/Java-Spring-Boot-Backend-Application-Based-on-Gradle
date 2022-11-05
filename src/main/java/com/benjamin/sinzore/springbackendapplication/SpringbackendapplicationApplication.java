1dd899d29c244ccd7d8196fa61719ba6524aa5f0
benjiesinzore <benjaminsinzore@gmail.com>
2022-11-05 13:05:32 +0300

    Initial Commit

---
+++ src/main/java/com/benjamin/sinzore/springbackendapplication/SpringbackendapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.benjamin.sinzore.springbackendapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbackendapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbackendapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/benjamin/sinzore/springbackendapplication/SpringbackendapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benjamin.sinzore.springbackendapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbackendapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
